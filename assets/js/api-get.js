cbe2df15208c86f7af819930265887eadd31e450
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-05-04 06:06:49 -0500

    finished?

--- assets/js/api-get.js
+++ assets/js/api-get.js
@@ -3,30 +3,18 @@ const nameSearchBar = document.querySelector("#name-search-bar");
 const tagSearchBar = document.querySelector("#tag-search-bar");
 
 const studentsApiUrl = "https://api.hatchways.io/assessment/students";
-
-const submitHandler = function(event) {
-    event.preventDefault();
-    studentsFetcher();
-};
-
-//My normal methods of simulating a pause in javascript was behaving either too aggressively
-//or not aggressively enough, so I've shamelessly stolen this bit from stackoverflow.
-//It's so much better than my method and is now my new method.
-const studentSearchBar = function(callback, wait) {
-    let timeout;
-    return (...args) => {
-        clearTimeout(timeout);
-        timeout = setTimeout(function () { callback.apply(this, args); }, wait);
-    };
-}
-
 let studentsStorage = {};
 
 const studentsFetcher = async function() {
     await fetch(studentsApiUrl)
         .then((res) => res.json())
         .then((data) => {
-            studentsStorage = data.students;
+            studentsStorage = data.students.map(arr => (
+                {...arr,
+                fullName: arr.firstName.toLowerCase() + " " + arr.lastName.toLowerCase(),
+                tags: []
+                }));
+
             for (i = 0; i < studentsStorage.length; i++) {
                 studentsOutput(studentsStorage[i]);
             };
@@ -36,20 +24,23 @@ const studentsFetcher = async function() {
 const studentsOutput = function(students) {
     const studentsDivEl = document.createElement("div");
     studentsDivEl.className = "students-storage-div";
-    studentsDivEl.id = "student" + i;
+    studentsDivEl.id = "student" + students.id;
 
 
     const studentsImgDivEl = document.createElement("div");
     studentsImgDivEl.className = "students-img-div";
+
     const studentsPicEl = document.createElement("img");
     studentsPicEl.setAttribute("src", students.pic);
+
     studentsImgDivEl.appendChild(studentsPicEl);
+
     studentsDivEl.appendChild(studentsImgDivEl);
 
     const studentsInfoDivEl = document.createElement("div");
 
     const studentsNameEl = document.createElement("h2");
-    studentsNameEl.textContent = students.firstName.toUpperCase() + " " + students.lastName.toUpperCase();
+    studentsNameEl.textContent = students.fullName.toUpperCase();
     studentsInfoDivEl.appendChild(studentsNameEl);
 
     const studentsEmailEl = document.createElement("p");
@@ -70,34 +61,43 @@ const studentsOutput = function(students) {
 
     const studentsTableDivEl = document.createElement("div");
     studentsTableDivEl.className = "students-table-div";
-    studentsTableDivEl.id = "student-table-div" + i;
+    studentsTableDivEl.id = "student-table-div" + students.id;
     studentsInfoDivEl.appendChild(studentsTableDivEl);
 
     const studentsTagsDivEl = document.createElement("div");
     studentsTagsDivEl.className = "students-tags-div";
-    studentsTagsDivEl.id = "student-tags-div" + i;
+    studentsTagsDivEl.id = "student-tags-div" + students.id;
 
     const studentsTagsStorageDivEl = document.createElement("div");
     studentsTagsStorageDivEl.className = "students-tags-storage-div";
-    studentsTagsStorageDivEl.id = "student-tags-storage-div" + i;
+    studentsTagsStorageDivEl.id = "student-tags-storage-div" + students.id;
+
+    studentsStorage[i].tags.map(arr => {
+        const studentTag = document.createElement("span");
+        studentTag.className = "student-tag";
+        studentTag.textContent = arr;
+
+        studentsTagsStorageDivEl.appendChild(studentTag);
+    });
+
     studentsTagsDivEl.appendChild(studentsTagsStorageDivEl);
 
-    const studentsTagsEntryDivEl = document.createElement("div");
-    studentsTagsEntryDivEl.className = "students-tags-entry-div";
-    studentsTagsEntryDivEl.id = "student-tags-entry-div" + i;
+    const studentsTagsEntryFormEl = document.createElement("form");
+    studentsTagsEntryFormEl.className = "students-tags-entry-form";
+    studentsTagsEntryFormEl.id = "student-tags-entry-form" + students.id;
+    studentsTagsEntryFormEl.setAttribute("onsubmit", "handleNewTag(" + students.id + ");return false")
+
 
     const studentsTagsEntryInputEl = document.createElement("input");
     Object.assign(studentsTagsEntryInputEl, {
-        className: "students-tags-entry-input",
-        id: "student-tags-entry-input" + i,
+        className: "search-bar",
+        id: "student-tags-entry-input" + students.id,
         type: "text",
         placeholder: "Add a tag",
-//        SubmitEvent: "handleNewTag("+i+")"
     });
-    studentsTagsEntryInputEl.setAttribute("submit", "handleNewTag(" + i + ")")   //("submit", "handleNewTag("+i+")");
 
-    studentsTagsEntryDivEl.appendChild(studentsTagsEntryInputEl);
-    studentsTagsDivEl.appendChild(studentsTagsEntryDivEl);
+    studentsTagsEntryFormEl.appendChild(studentsTagsEntryInputEl);
+    studentsTagsDivEl.appendChild(studentsTagsEntryFormEl);
 
     studentsInfoDivEl.appendChild(studentsTagsDivEl);
 
@@ -106,15 +106,45 @@ const studentsOutput = function(students) {
     studentsDivEl.appendChild(studentsInfoDivEl);
 
     const studentsGradesButtonEl = document.createElement("button");
+    studentsGradesButtonEl.id = "test-toggle" + students.id;
     studentsGradesButtonEl.textContent = "+";
     studentsGradesButtonEl.type = "button";
-    studentsGradesButtonEl.setAttribute("onclick", "handleTestsButtonClick(" + i + ")");
+    studentsGradesButtonEl.setAttribute("onclick", "handleTestsButtonClick(" + students.id + ")");
 
     studentsDivEl.appendChild(studentsGradesButtonEl);
 
     container.appendChild(studentsDivEl);
 }
 
+const handleNewTag = function(id) {
+    const studentTag = document.createElement("span");
+    studentTag.className = "student-tag";
+    studentTag.textContent = document.querySelector("#student-tags-entry-input" + id).value;
+
+    const studentTagStorageDiv = document.querySelector("#student-tags-storage-div" + id);
+    studentTagStorageDiv.appendChild(studentTag);
+
+    studentsStorage.find(arr => arr.id === String(id)).tags.push(document.querySelector("#student-tags-entry-input" + id).value);
+}
+
+//My normal methods of simulating a pause in javascript was behaving either too aggressively
+//or not aggressively enough, so I've shamelessly stolen this bit from stackoverflow.
+//It's so much better than my method and is now my new method.
+const studentSearchBar = function(callback, wait) {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () { callback.apply(this, args); }, wait);
+    };
+}
+
+nameSearchBar.addEventListener("keyup", studentSearchBar(() => {
+    studentCombinedSearch();
+}, 250))
+
+tagSearchBar.addEventListener("keyup", studentSearchBar(() => {
+    studentCombinedSearch();
+}, 250))
 
 const studentsOutputCleaner = function() {
     while(container.getElementsByClassName("students-storage-div").length > 0) {
@@ -122,39 +152,44 @@ const studentsOutputCleaner = function() {
     };    
 }
 
-const filteredStudents = function() {
-    const searchString = nameSearchBar.value.toLowerCase();
+const studentCombinedSearch = function() {
+    const searchName = nameSearchBar.value.toLowerCase();
+    const searchTag = tagSearchBar.value;
 
-    for (i = 0; i < studentsStorage.length; i++) {
-        studentsStorage[i].fullName = studentsStorage[i].firstName.toLowerCase() + " " + studentsStorage[i].lastName.toLowerCase();
-    };
-
-    for (i = 0; i < studentsStorage.length; i++) {
-        if (studentsStorage[i].fullName.includes(searchString)) {
-            studentsOutput(studentsStorage[i]);
-            console.log(studentsStorage[i]);
-        }
-    };
-}
-
-nameSearchBar.addEventListener("keyup", studentSearchBar(() => {
-    console.log(nameSearchBar.value);
     studentsOutputCleaner();
-    filteredStudents();
-}, 250));
 
-const handleTestsButtonClick = function(id) {
+    if(searchTag) {
+        for (i = 0; i < studentsStorage.length; i++) {
+            if (studentsStorage[i].fullName.includes(searchName) && studentsStorage[i].tags.map(arr => arr.includes(String(searchTag))).includes(true)) {
+                studentsOutput(studentsStorage[i]);
+                console.log(studentsStorage[i]);
+            }
+        };
+    }
+
+    if(!searchTag){
+        for (i = 0; i < studentsStorage.length; i++) {
+            if (studentsStorage[i].fullName.includes(searchName)) {
+                studentsOutput(studentsStorage[i]);
+                console.log(studentsStorage[i]);
+            }
+        };
+    }
+
+};
+
+const handleTestsButtonClick = function(id) { 
     if (!document.querySelector("#student-table" + id)) {
         const studentTestTable = document.createElement("table");
         studentTestTable.id = "student-table"+ id;
 
-        for (i = 0; i < studentsStorage[id].grades.length; i++) {
+        for (i = 0; i < studentsStorage.find(arr => arr.id === String(id)).grades.length; i++) {
             const studentTestRow = document.createElement("tr");
             const studentCellOne = document.createElement("td");
             const studentCellTwo = document.createElement("td");
 
-            studentCellOne.textContent = "Test " + (i + 1) + ":";
-            studentCellTwo.textContent = studentsStorage[id].grades[i] + "%";
+            studentCellOne.textContent = "Test " + (i + 1) + ":    ";
+            studentCellTwo.textContent = studentsStorage.find(arr => arr.id === String(id)).grades[i] + "%";
 
             studentTestRow.appendChild(studentCellOne);
             studentTestRow.appendChild(studentCellTwo);
@@ -162,26 +197,16 @@ const handleTestsButtonClick = function(id) {
             studentTestTable.appendChild(studentTestRow);
         };
 
-        const studentTableDiv = document.querySelector("#student-table-div" + id)    
+        const studentTableDiv = document.querySelector("#student-table-div" + id);    
         studentTableDiv.appendChild(studentTestTable);
+        
+        document.querySelector("#test-toggle" + id).textContent = "-";
     } else {
         const element = document.querySelector("#student-table" + id);
         element.parentNode.removeChild(element);
-    };
-};
 
-const handleNewTag = function(id) {
-    handleTestsButtonClick
-    //if (document.getElementById("student-tags-entry-input" + id).value) {
-       // event.preventDefault();
-
- //       const studentTag = document.createElement("p");
-   //     studentTag.className = "student-tag";
-     //   studentTag.textContent = document.querySelector("#student-tags-entry-input" + id).value;
-
- //       const studentTagStorageDiv = document.querySelector("#student-tags-storage-div" + id);
-   //     studentTagStorageDiv.appendChild(studentTag);
-  //  };
+        document.querySelector("#test-toggle" + id).textContent = "+";
+    }
 };
 
 studentsFetcher();
